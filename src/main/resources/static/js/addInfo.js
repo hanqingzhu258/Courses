a345168ed36f73039c7ab48c5baf0d202cca6652
hanqingzhu <zhuhanqing258@foxmail.com>
2018-01-20 12:29:16 +0800

    change some js's name

---
+++ src/main/resources/static/js/addInfo.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    var E = window.wangEditor;
+    var editor = new E('#homeworkContent');
+    // 或者 var editor = new E( document.getElementById('editor') )
+    editor.create()
+});
